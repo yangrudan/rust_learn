b35a0d1710446840a7d1e9878b2010b5ea30e967
Cookie <1549029765@qq.com>
2024-01-18 22:27:43 +0800

    输入一个数字

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("猜数游戏!");
+
+    let mut guess = String::new();
+    match io::stdin().read_line(&mut guess) {
+        Ok(n) => {
+            println!("{n} bytes read");
+            println!("{guess}");
+        }
+        Err(error) => println!("error: {error}"),
+    }
 }
