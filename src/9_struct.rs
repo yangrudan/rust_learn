ac9d3bc43216a0b9ede0072be01f7d32c80e1dee
Cookie <1549029765@qq.com>
2024-01-23 11:19:27 +0800

    struct

---
+++ src/9_struct.rs
@@ -0,0 +1,28 @@
+struct User{
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+//Tuple Struct
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+//unit-like struct  (没有field content)
+
+
+fn main() {
+    let mut user = User {
+        email: String::from("154765@qq.com"),
+        username: String::from("Yang"),
+        active:true,
+        sign_in_count: 556,
+    };
+
+    println!("{}", user.email);
+
+
+    user.email = String::from("154765@qq.com");
+}
+
