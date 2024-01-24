92f302d7df012fdcf23765a5355e301abf9681c2
Cookie <1549029765@qq.com>
2024-01-24 11:10:35 +0800

    Option<T>

---
+++ src/12_绑定值的匹配.rs
@@ -0,0 +1,36 @@
+//绑定值的匹配
+//Option<T> 有 None
+
+fn main() {
+    let c = Coin::Quarter(UsState::Alabama);
+    println!("{}", value_in_cents(c));
+}
+
+#[derive(Debug)]
+enum Coin{
+    Penny,
+    Nickle,
+    Dime,
+    Quarter(UsState),  //关联一个数据
+}
+
+#[derive(Debug)]
+enum UsState{
+    Alabama,
+    Alaska,
+}
+
+fn value_in_cents(coin: Coin) -> u8{
+    match coin {
+        Coin::Penny => {
+            println!("Penny");
+            1
+        },
+        Coin::Nickle => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,3 @@
 fn main() {
 
 }
-
-enum Coin{
-    Penny,
-    Nickle,
-    Dime,
-    Quarter,
-}
-
-
-fn value_in_cents(coin: Coin) -> u8{
-    match coin {
-        Coin::Penny => {
-            println!("Penny");
-            1
-        },
-        Coin::Nickle => 5,
-        Coin::Dime => 10,
-        Coin::Quarter => 25,
-    }
-}
