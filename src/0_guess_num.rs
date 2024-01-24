892f6d32816576b665fc110b8d7b2694be014810
Cookie <1549029765@qq.com>
2024-01-24 15:17:20 +0800

    ctrl+shift_7 format file

--- src/0_guess_num.rs
+++ src/0_guess_num.rs
@@ -2,13 +2,12 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
-
 fn main() {
     println!("猜数游戏!");
-    let secret_number = rand::thread_rng().gen_range(1,101);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("神秘数字是: {}", secret_number);
 
-    loop{
+    loop {
         let mut guess = String::new();
         match io::stdin().read_line(&mut guess) {
             Ok(n) => {
@@ -18,7 +17,7 @@ fn main() {
             Err(error) => {
                 println!("error: {error}");
                 continue;
-            },
+            }
         }
 
         let guess: u32 = match guess.trim().parse() {
@@ -26,7 +25,7 @@ fn main() {
             Err(_) => continue,
         };
         println!("你猜测的数字是:{}", guess);
-        match  guess.cmp(&secret_number){
+        match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small"),
             Ordering::Greater => println!("Too big"),
             Ordering::Equal => {
