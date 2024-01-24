b1108c9679d26f79b2108b4cc9ba1fe4a59e43a8
Cookie <1549029765@qq.com>
2024-01-24 14:57:23 +0800

    Vector

---
+++ src/15_集合.rs
@@ -0,0 +1,29 @@
+fn main() {
+    let mut v = Vec::new();
+
+    //更新vector
+    v.push(1);
+
+    for i in &v{
+        println!("{}", i);
+    }
+
+
+    //Excel单元格, 利用enum存放多种数据类型
+    let row = vec![
+        SpreadSheetCell::Int(4),
+        SpreadSheetCell::Text(String::from("blue")),
+        SpreadSheetCell::Float(10.12),
+    ];
+
+    for i in &row{
+        println!("{:?}", i);
+    }
+}
+
+#[derive(Debug)]
+enum SpreadSheetCell {
+    Int(i32),
+    Float(f64),
+    Text(String),
+}
