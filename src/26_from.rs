e5a3b8cd33e6a61f91f1e8d2d67ad289a58a389e
Cookie <1549029765@qq.com>
2024-01-29 16:12:24 +0800

    from

---
+++ src/26_from.rs
@@ -0,0 +1,34 @@
+// Copyright (c) Cookie Yang. All right reserved.
+fn main() {
+    let mut _mutable_integer = 7i32;
+
+    {
+        // 被不可变的 `_mutable_integer` 遮蔽
+        let _mutable_integer = _mutable_integer;
+
+        // 报错！`_mutable_integer` 在本作用域被冻结
+        // _mutable_integer = 50;
+        // 改正 ^ 注释掉上面这行
+
+        // `_mutable_integer` 离开作用域
+    }
+
+    // 正常运行！ `_mutable_integer` 在这个作用域没有冻结
+    _mutable_integer = 3;
+
+    use std::convert::From;
+
+    #[derive(Debug)]
+    struct Number {
+        value: i32,
+    }
+
+    impl From<i32> for Number {
+        fn from(item: i32) -> Self {
+            Number { value: item }
+        }
+    }
+
+    let num = Number::from(30);
+    println!("My number is {:?}", num);
+}
