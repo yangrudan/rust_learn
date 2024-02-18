93bce2eb9b94eca4d3b9f6938bf0a4d598608099
Cookie <1549029765@qq.com>
2024-02-18 10:34:48 +0800

    所有权

--- examples/5_所有权.rs
+++ examples/5_所有权.rs
@@ -20,13 +20,12 @@ fn main() {
     println!("{}", x);
 }
 
-
-fn takes_ownership(some_string: String) { // some_string 进入作用域
+fn takes_ownership(some_string: String) {
+    // some_string 进入作用域
     println!("{}", some_string);
 } // 这里，some_string 移出作用域并调用 `drop` 方法。占用的内存被释放
 
-fn makes_copy(some_integer: i32) { // some_integer 进入作用域
+fn makes_copy(some_integer: i32) {
+    // some_integer 进入作用域
     println!("{}", some_integer);
 } // 这里，some_integer 移出作用域。不会有特殊操作
-
-;
