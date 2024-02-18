58d6f2209f668e6c8bfac309f507fdaeed3b221b
Cookie <1549029765@qq.com>
2024-02-18 10:56:55 +0800

    切片

--- examples/8_切片.rs
+++ examples/8_切片.rs
@@ -1,20 +1,20 @@
 fn main() {
-    let mut s = String::from("Hello World");
+    let s = String::from("Hello World");
 
-    let hello = &s[0..5];  // 引用一部分
+    let hello = &s[0..5]; // 引用一部分
     let world = &s[6..];
 
     print!("{},{}", hello, world);
 
-    let wordIndex = first_world(&s);
-    println!("{}", wordIndex)
+    let word_index = first_world(&s);
+    println!("{}", word_index)
 }
 
 fn first_world(s: &String) -> &str {
     let bytes = s.as_bytes();
 
-    for(i, &item) in bytes.iter().enumerate(){
-        if item == b' '{
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
             return &s[..i];
         }
     }
