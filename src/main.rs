d924555ebbfa36eb301a0b59257227ec10361452
Cookie <1549029765@qq.com>
2024-02-19 11:29:56 +0800

    泛型实现

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,29 @@ fn give_me<T: std::fmt::Display>(value: T) {
     println!("{}", a);
 }
 
+struct Container<T> {
+    item: T,
+}
+
+impl<T> Container<T> {
+    fn new(item: T) -> Self {
+        Container { item }
+    }
+}
+
+impl Container<u32> {
+    fn sum(item: u32) -> Self {
+        println!("hello");
+        Container { item }
+    }
+}
+
 fn main() {
     let a = "aaa";
     let b = 1;
     give_me(a);
     give_me(b);
+
+    let _c = Container::sum(32u32);
+    let _d = Container::new(true);
 }
