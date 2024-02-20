bcaf50b2758ba2d8fcadd4b65052d8781fd75e9d
Cookie <1549029765@qq.com>
2024-02-20 10:37:48 +0800

    指针; 单链表

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,55 @@
-use std::fmt::Debug;
-use std::task::ready;
+use std::rc::Rc;
+use std::rc::Weak;
 
 #[derive(Debug)]
-struct Square(f32);
-
-#[derive(Debug)]
-struct Rectangle(f32, f32);
-
-trait Area: Debug {
-    fn get_area(&self) -> f32;
+struct LinkedList<T> {
+    head: Option<Rc<Node<T>>>,
 }
 
-impl Area for Square {
-    fn get_area(&self) -> f32 {
-        self.0 * self.0
+#[derive(Debug)]
+struct Node<T> {
+    next: Option<Rc<Node<T>>>,
+    data: T,
+}
+
+impl<T> LinkedList<T> {
+    fn new() -> Self {
+        LinkedList { head: None }
     }
-}
 
-impl Area for Rectangle {
-    fn get_area(&self) -> f32 {
-        self.0 * self.1
+    fn append(&self, data: T) -> Self {
+        LinkedList {
+            head: Some(Rc::new(Node {
+                next: self.head.clone(),
+                data,
+            })),
+        }
     }
 }
 
 fn main() {
-    let shapes: Vec<&dyn Area> = vec![&Square(3f32), &Rectangle(4f32, 2f32)];
-    for s in shapes {
-        println!("{:?}", s.get_area());
-    }
+    ///原始指针
+    let a = &56;
+    let a_raw_ptr = a as *const i32;
+    let mut b = &mut 555;
+    let mut b_mut_ptr = b as *mut i32;
+    b = &mut 33;
+
+    ///Box<T>
+    let box_one = Box::new(Foo);
+    let un_box_one = *box_one;
+    box_ref(un_box_one);
+
+    ///引用计数
+    let list_of_number = LinkedList::new().append(1).append(2);
+    println!("{:?}", list_of_number);
+
+    println!("end");
 }
+
+fn box_ref<T>(b: T) -> Box<T> {
+    let a = b;
+    Box::new(a)
+}
+
+struct Foo;
